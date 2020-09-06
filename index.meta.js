5950d237e7aaa8a21bf4b2f918e3af26aed74edc
boomerange2333 <70745620+boomerange2333@users.noreply.github.com>
2020-09-06 15:14:06 +0800

    Create index.meta.js

---
+++ index.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        approval-migrator
+// @version     0.0.4
+// @match       *://aflow.dingtalk.com/*
+// @match       *://oa.dingtalk.com/*
+// @run-at      document-start
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_deleteValue
+// @grant       GM_listValues
+// @grant       GM_addValueChangeListener
+// @grant       GM_removeValueChangeListener
+// @grant       GM_openInTab
+// @grant       GM_registerMenuCommand
+// @grant       GM_download
+// @grant       unsafeWindow
+// @updateURL   https://cdn.jsdelivr.net/gh/boomerange2333/approval-migrator/index.meta.js
+// @downloadURL https://cdn.jsdelivr.net/gh/boomerange2333/approval-migrator/index.user.js
+// @noframes    
+// ==/UserScript==
