d3fc8fbb6ed5f2b0042f2b3e9252669e80e8bcc8
boomerange2333 <70745620+boomerange2333@users.noreply.github.com>
2021-07-12 18:23:32 +0800

    修复部分数据量大的用户导出失败的问题

--- index.meta.js
+++ index.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        通用网页信息获取工具
-// @version     0.1.0
+// @version     0.1.3
 // @match       *://aflow.dingtalk.com/*
 // @match       *://oa.dingtalk.com/*
 // @run-at      document-start
@@ -16,5 +16,5 @@
 // @grant       unsafeWindow
 // @updateURL   https://cdn.jsdelivr.net/gh/boomerange2333/approval-migrator/index.meta.js
 // @downloadURL https://cdn.jsdelivr.net/gh/boomerange2333/approval-migrator/index.user.js
-// @noframes    
+// @noframes
 // ==/UserScript==
